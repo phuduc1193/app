44d360f37f06a2d35406fddcbd5d47a2f67a7972
Duc Truong <phuduc1193@gmail.com>
2017-07-16 17:51:03 +0700

    Login form sent

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -5,11 +5,13 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     passport    = require('./common/passport'),
     apiRouter   = require('./router/api'),
+    cors        = require('cors'),
     app         = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -9,24 +10,18 @@ import 'rxjs/add/operator/map';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
-  user;
 
-  constructor(private _fb: FormBuilder, private _http: Http) {
-    this.user = {
-      username: '',
-      password: ''
-    };
-  }
+  constructor(private _fb: FormBuilder, private _http: Http) { }
 
   ngOnInit() {
     this.form =  this._fb.group({
-      username: [this.user.username, [Validators.required, Validators.pattern('(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$')]],
-      password: [this.user.password, [Validators.required, Validators.pattern('^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$')]],
+      username: ['', [Validators.required, Validators.pattern('(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$')]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$')]],
     });
   }
 
   login() {
-    this._http.post('localhost:3000/api/auth/login', this.user)
+    this._http.post(environment.apiUrl + 'auth/login', this.form.value)
       .map(res => res.json())
       .subscribe(result => console.log(result));
   }
