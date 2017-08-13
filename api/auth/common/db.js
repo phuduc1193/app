54190fb6ea6b477db0e2b07937f31a5fd3c710bd
Duc Truong <phuduc1193@gmail.com>
2017-08-13 14:20:35 +0700

    Revamp App

--- api/auth/common/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-//DB setup
-var db = mongoose.connect(process.env.DATABASE_URL, { useMongoClient: true });
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-module.exports = db;
